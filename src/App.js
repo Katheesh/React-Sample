88175b4b876cf220368bfaa5b0a5eeb7bb63483e
Katheeskumar Sithamparapillai <katheeskumar@outlook.com>
2020-06-01 10:08:14 +0530

    basic d=terminal added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="console">
+        <span id="a">katheesh@linux</span>:<span id="b">~</span><span id="c">$</span> cd Katheesh/InfoSite <br/>
+        <span id="a">katheesh@linux</span>:<span id="b">~</span><span id="c">$</span> cat myweb.html<br/><br/>
+        My favorite code quote, "<span id="k">Efficiency consists in a resolute mind.</span>". <br/><br/>
+
+        Hey There! I am Katheeskumar (SOFTWARE ENGINEER & ENTREPRENEUR) 🇱🇰. 
+        <p>I am currently working as freelancing.</p>
+
+        <a href="https://www.npmjs.com/package/@katheesh/mailer-js"><pre> @katheesh/mailer-js </pre></a>
+        <a href="https://www.npmjs.com/package/vue-mailer"><pre> vue-mailer </pre></a>Package Collaborator
+ 
+
+        <a href="https://www.patreon.com/bePatron?u=32135007">Become a Patron!</a>
+
+        <p> I love to coding and have done so for 10,000 hours over the past four years. I’m great at knowing the right algorithm for a problem because I read computer science literature constantly. I like Algorithms based problems. I love working with people to do things bigger than I could accomplish alone. I’m motivated by big problems, and I think you’ve got some here that I can help solve.”</p>
+        
+        <p> You can visit my other profiles, here: </p> 
+        <a href="https://www.linkedin.com/in/katheeskumar/">LinkedIn</a> <br/> 
+        <a href="https://github.com/katheesh">GitHub</a>
+
+        <br/><br/>
+        If you like to visit my official GUI Website <a href ="https://katheesh.gitleaf.com">Click here </a><br/><br/>
+
+        If you would like to get in touch with me , feel free to send me an email at <a href="mailto: ikatheesh@gmail.com">ikatheesh@gmail.com</a>.
+        
+        <p>Cheers!</p>
+        <span id="a">katheesh@linux</span>:<span id="b">~</span><span id="c">$</span> exit_
+
+      </div> 
     </div>
   );
 }
