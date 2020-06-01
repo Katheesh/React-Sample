790335b2f321af7ca32fd98dcf42d42c2580b743
Katheeskumar Sithamparapillai <katheeskumar@outlook.com>
2020-06-01 10:20:32 +0530

    basic d=terminal added

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
 
         Hey There! I am Katheeskumar (SOFTWARE ENGINEER & ENTREPRENEUR) 🇱🇰. 
         <p>I am currently working as freelancing.</p>
-
-        <a href="https://www.npmjs.com/package/@katheesh/mailer-js"><pre> @katheesh/mailer-js </pre></a>
+        
+        <a href="https://www.npmjs.com/package/@katheesh/mailer-js"><pre> mailer-js </pre></a> &
         <a href="https://www.npmjs.com/package/vue-mailer"><pre> vue-mailer </pre></a>Package Collaborator
  
-
+        <br/><br/>
         <a href="https://www.patreon.com/bePatron?u=32135007">Become a Patron!</a>
 
         <p> I love to coding and have done so for 10,000 hours over the past four years. I’m great at knowing the right algorithm for a problem because I read computer science literature constantly. I like Algorithms based problems. I love working with people to do things bigger than I could accomplish alone. I’m motivated by big problems, and I think you’ve got some here that I can help solve.”</p>
@@ -30,7 +30,7 @@ function App() {
         If you would like to get in touch with me , feel free to send me an email at <a href="mailto: ikatheesh@gmail.com">ikatheesh@gmail.com</a>.
         
         <p>Cheers!</p>
-        <span id="a">katheesh@linux</span>:<span id="b">~</span><span id="c">$</span> exit_
+        <span id="a">katheesh@linux</span>:<span id="b">~</span><span id="c">$</span> exit
 
       </div> 
     </div>
